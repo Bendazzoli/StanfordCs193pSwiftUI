1a545c25c301ece5ccd5f4954545aaa11a65f003
Paulo Henrique Bendazzoli <Paulo.Bendazzoli-ext@ab-inbev.com>
2021-05-24 20:16:55 -0300

    Start project

---
+++ Memorize/Memorize/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by Paulo Henrique Bendazzoli on 24/05/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
