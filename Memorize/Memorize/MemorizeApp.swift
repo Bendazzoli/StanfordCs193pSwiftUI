2a48c7b758682b1730fd62744eead6a78c159cb2
Paulo Henrique Bendazzoli <Paulo.Bendazzoli-ext@ab-inbev.com>
2023-02-06 21:35:43 -0300

    Start project with HStack and ZStack view

---
+++ Memorize/Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Paulo Henrique Bendazzoli on 06/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
